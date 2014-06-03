d00531df8aa7ad95a1fc90d211c0761f51804263
Edson Tirelli <ed.tirelli@gmail.com>
2014-06-03 15:33:45 -0400

    Implementing rest interface and trying out camel integration

--- kie-server/src/main/java/org/kie/server/impl/KieServerImpl.java
+++ kie-server/src/main/java/org/kie/server/impl/KieServerImpl.java
@@ -1,46 +1,30 @@
 package org.kie.server.impl;
 
-import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.jws.WebService;
 
-import org.kie.api.KieServices;
-import org.kie.api.builder.ReleaseId;
-import org.kie.api.runtime.KieContainer;
+import org.drools.core.command.impl.GenericCommand;
+import org.kie.api.command.Command;
+import org.kie.api.runtime.CommandExecutor;
 import org.kie.server.api.KieContainerInfo;
-import org.kie.server.api.KieServer;
-import org.kie.server.api.ServiceResponse;
 
-@WebService(endpointInterface="org.kie.server.api.KieServer", serviceName="KieServer")
-public class KieServerImpl implements KieServer {
+//@WebService(endpointInterface="org.kie.server.api.KieServer", serviceName="KieServer")
+public class KieServerImpl implements CommandExecutor {
     private final Map<String, KieContainerInfo> containers = new ConcurrentHashMap<String, KieContainerInfo>();
 
-    @Override
-    public ServiceResponse deployModule(String containerId, ReleaseId releaseId) {
-        KieServices ks = KieServices.Factory.get();
-        KieContainer kieContainer = ks.newKieContainer(releaseId);
-        if( kieContainer != null ) {
-            containers.put(containerId, new KieContainerInfoImpl(containerId, KieContainerInfo.Status.STARTED, kieContainer));
-            return new ServiceResponse(ServiceResponse.ResponseType.SUCCESS, "Container "+containerId+" successfully deployed with KJar "+releaseId+".");
-        } else {
-            return new ServiceResponse(ServiceResponse.ResponseType.FAILURE, "Failed to create container "+containerId+" with KJar "+releaseId+".");
-        }
-    }
+//    @POST
+//    @Path("/execute")
+//    @Produces({"application/xml","application/json"})
+//    @Consumes({"application/xml","application/json","application/x-www-form-urlencoded"})
+//    public <T> T execute(GenericCommand<T> command) {
+//        return ;
+//    }
 
     @Override
-    public Collection<KieContainerInfo> getModules() {
-        return containers.values();
-    }
-
-    @Override
-    public ServiceResponse undeployModule(String containerId) {
-        if( containers.containsKey(containerId) ) {
-            containers.remove(containerId);
-            return new ServiceResponse(ServiceResponse.ResponseType.SUCCESS, "Container "+containerId+" successfully undeployed.");
-        }
-        return new ServiceResponse(ServiceResponse.ResponseType.FAILURE, "Container "+containerId+" was not deployed.");
+    public <T> T execute(Command<T> command) {
+        return ((GenericCommand<T>)command).execute(null);
     }
 
 }
